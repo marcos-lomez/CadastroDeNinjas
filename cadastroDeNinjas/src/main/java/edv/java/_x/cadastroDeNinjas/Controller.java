5afd208b34c21c70d8a204b0a55b1cf889622cef
marcos lomez <marcos.lomez@gmail.com>
2025-08-04 20:12:53 -0300

    Criado a contorller [aula02]

---
+++ cadastroDeNinjas/src/main/java/edv/java/_x/cadastroDeNinjas/Controller.java
@@ -0,0 +1,16 @@
+package edv.java._x.cadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+    @GetMapping("/boasVindas")
+    public String BoaVindas(){
+        return "essa e minha primeira mensagem nessa rota";
+    }
+
+
+}
