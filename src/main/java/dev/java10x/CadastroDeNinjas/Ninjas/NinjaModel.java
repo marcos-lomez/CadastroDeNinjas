599d2f380528350212ee0f2d0a0b36664d8d797e
marcos lomez <marcos.lomez@gmail.com>
2026-01-26 22:10:47 -0300

    fix:Adicionar o @Columm na minha Classe

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -1,4 +1,4 @@
-package dev.java10x.CadastroDeNinjas.Ninjas.Controller;
+package dev.java10x.CadastroDeNinjas.Ninjas;
 import dev.java10x.CadastroDeNinjas.missoes.MisooesModel;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -16,16 +16,23 @@ public class NinjaModel {
 //alteração
    @Id
    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
    long id;
+   @Column(name = "nome")
    private String nome;
 
    @Column(unique = true)
    private String email;
 
+   @Column(name = "idade")
    private int idade;
+
+    @Column(name = "imgUrl")
+   private String imgUrl;
 
    @ManyToOne
    @JoinColumn(name="missoes_id") // foreina key ou chave estrangeira
    private List<MisooesModel> missoes;
+
 
 }
---
+++ src/main/java/dev/java10x/CadastroDeNinjas/missoes/MissoesRepository.java
@@ -0,0 +1,14 @@
+package dev.java10x.CadastroDeNinjas.missoes;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissoesRepository extends JpaRepository<MisooesModel,Long>{
+
+
+
+
+
+
+
+
+
+}
