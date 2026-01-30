8f47bc33a12baca3bc4b5f7c68184ada985fb4ba
marcos lomez <marcos.lomez@gmail.com>
2026-01-30 12:02:03 -0300

    feat:Atualizar o controller com mok de dados

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,8 +1,6 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -14,8 +12,40 @@ public String BoasVindas(){
     return "essa e minha primeira menssagem nessa rotA";
 }
 
+//Adicionar Ninja (CREATE)
+  @PostMapping("/criar")
+    public String criarNinja(){
+        return "Ninja Criado";
+
+  }
+
+//Procura Ninja por ID  (create)
+@GetMapping("/todosID")
+public String MostrarTodosPorID(){
+    return  "Mostrar Ninja POr ID";
+
+}
 
 
+//Mostrar todos os ninjas (READ)
+ @GetMapping("/todos")
+    public String MostrarTodos(){
+    return  "Mostarr Ninja";
+
+ }
 
 
+//Alterar dados ninjas (UPDATE)
+@PutMapping("/alterarId")
+    public String alterarNinjaPorId(){
+        return "Alterar Ninja por ID";
+
+}
+
+//Deletar Ninja (DELETE)
+@DeleteMapping("/deletarID")
+    public String deletarNinjaPorId(){
+        return "deletado  por id";
+
+}
 }
